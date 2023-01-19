bdcbf81b3b2b4bdb5771519c9f652faa2952c0b1
Orphee <orphee.lonie@gmail.com>
2023-01-19 17:21:20 +0100

    [Solver] use nb_random_swap and rollback_nb_swap + add DEBUG

--- solver.cc
+++ solver.cc
@@ -5,6 +5,8 @@
 #include <cmath>
 #include <algorithm>
 
+//#define DEBUG
+
 namespace tetravex
 {
     Solver::Solver(Board *b)
@@ -24,41 +26,48 @@ namespace tetravex
     {
         std::random_device rd{};
         std::mt19937 gen{rd()};
-        std::normal_distribution<> clock{0, double(board->size)};
+        std::normal_distribution<> clock{0, 2};
 
         double last_cost = board->cost();
         double new_cost;
-        double T = board->size * 111;
-        double min_t = 0.4; //1 - board->size / 10;
-        //int i = 0;
+        double T = 350; // board->size * 111;
+        const double min_t = 0.45; //1 - board->size / 10;
+        const double coef_t = 0.99;
+
+#ifdef DEBUG
+        int i = 0;
+#endif
 
         while(last_cost != 0)
         {
             // int nb_swap = round(1 + std::abs(clock(gen)));
-            auto move = board->random_swap();
+            int nb_swap = 1;
+            auto move = board->nb_random_swap(nb_swap);
+            // auto move = board->random_swap();
 
             new_cost = board->cost();
             // double alpha = new_cost / last_cost;
 
 
-            /*
+#ifdef DEBUG
             i++;
             if (i%10000 == 0)
                 std::cout << i << ": " << last_cost << "\n";
-                */
+#endif
 
             //if (u <= alpha)
             if (new_cost < last_cost ||
                     std::exp((last_cost - new_cost)/T) > (double(std::rand()) / RAND_MAX))
             {
-                T *= 0.9999;
+                T *= coef_t;
                 T = std::max(T, min_t);
 
                 last_cost = new_cost;
             }
             else
             {
-                board->rollback_swap(move);
+                board->rollback_nb_swap(move);
+                //board->rollback_swap(move);
             }
         }
 
