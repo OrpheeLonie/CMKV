12f30d03d1921c1e0095611308dc3fba1120bf3d
m4riu <marius.hurbin@epita.fr>
2023-01-19 19:37:27 +0100

    best parameter 4x4 and 6x6

--- main.cc
+++ main.cc
@@ -29,21 +29,21 @@ int main()
     // // solver->board->pretty_print(std::cout);
 
     // delete solver;
-
-    // try different temperatures and degradation rates and see what is the quickest
-    // the different temperatures and degradation rates are in two lists below
-    // make a list of double for the temperatures and a list of double for the degradation rates
     
-    double temperatures[] = {325, 350, 375};
-    double degradation_rates[] = {0.99, 0.995, 0.985};
-    double min_temperatures[] = {0.425, 0.45, 0.475};
+    // best for 4x4: 375, 0.995, 0.45
+    // best for 5x5: 475, 0.99, 0.4 pas vraiment tester mais osef
+    // best for 6x6: 575, 0.9875, 0.375 => <30s atteint avec pur board sans case fixe
+
+    double temperatures[] = {575};
+    double degradation_rates[] = {0.9875, .99};
+    double min_temperatures[] = {.3625, .375};
 
     // make a vector of string containing the addresses of the files
     std::vector<std::string> paths;
     for (int i = 1; i < 10; i++){
         std::ostringstream ss;
         ss << i;
-        paths.push_back("data/input/s4-0" + ss.str() + ".txt");
+        paths.push_back("data/input/s6-0" + ss.str() + ".txt");
     }
 
     
@@ -63,29 +63,30 @@ int main()
                 double d = degradation_rates[k];
                 double m = min_temperatures[l];
 
+                for (auto h = 0; h < 3; h++){
+                    auto start = std::chrono::system_clock::now();
+                    for (int i = 0; i < 10; i++) {
+                        tetravex::Solver *solver = new tetravex::Solver(paths[1]);
+                        // std::cout << "Solver's board "<< i << " started!\n";
+                        // solver->board->pretty_print(std::cout);
 
-                auto start = std::chrono::system_clock::now();
-                for (int i = 0; i < 70; i++) {
-                    tetravex::Solver *solver = new tetravex::Solver(paths[i%9]);
-                    // std::cout << "Solver's board "<< i << " started!\n";
-                    // solver->board->pretty_print(std::cout);
+                        solver->no_probleme(t, d, m);
+                        // solver->board->pretty_print(std::cout);
 
-                    solver->no_probleme(t, d, m);
-                    // solver->board->pretty_print(std::cout);
+                        delete solver;
+                    }
+                    auto end =  std::chrono::system_clock::now();
+                    std::chrono::duration<double> elapsed_seconds = end-start;
 
-                    delete solver;
+                    if (elapsed_seconds < best_time) {
+                        best_time = elapsed_seconds;
+                        best_temperature = t;
+                        best_degradation_rate = d;
+                        best_min_temperature = m;
+                    }
+                    std::cout << "Time for temperature " << t << ", degradation rate " << d << ", and minimum temperature " << m << ": " << elapsed_seconds.count() << "\n";
+
                 }
-                auto end =  std::chrono::system_clock::now();
-                std::chrono::duration<double> elapsed_seconds = end-start;
-
-                if (elapsed_seconds < best_time) {
-                    best_time = elapsed_seconds;
-                    best_temperature = t;
-                    best_degradation_rate = d;
-                    best_min_temperature = m;
-                }
-                std::cout << "Time for temperature " << t << ", degradation rate " << d << ", and minimum temperature " << m << ": " << elapsed_seconds.count() << "\n";
-
             }
         }
     } 
